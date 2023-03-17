74d254cf4fa05b4e2f53c50be8b4d2e070796bd6
Ian Renton <ian@ianrenton.com>
2023-03-17 16:38:43 +0000

    Use MPU in DMP mode and base heading off Tait-Bryan angles rather than raw magnetometer

--- heading_nmea_udp_sender.c
+++ heading_nmea_udp_sender.c
@@ -3,14 +3,16 @@
 // https://github.com/ianrenton/beaglebone-blue-heading-nmea-udp-sender
 // Based on the MPU example from the Beaglebone Robot Control Library.
 //
-// Reads data from a Beaglebone Blue magnetometer, formats it as an NMEA-0183
-// HDT message, and sends it via UDP. Maybe useful for robotics software that
+// Reads data from a Beaglebone Blue motion processor, formats it as an NMEA-0183
+// GPHDT message, and sends it via UDP. Maybe useful for robotics software that
 // expects that format of heading data. This can also be ingested into `gpsd`,
 // by e.g. adding `udp://0.0.0.0:2021` to its list of sources.
 //
 // If using this for yourself, you may need to customise the #define values
 // near the top of the file to reflect the orientation of your board in your
 // robot.
+//
+// Must be run as root.
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -32,9 +34,6 @@
 // magnetometer-based heading to give the result. So if +X points to the left of your robot,
 // this should be 90. If +X points to the right, -90. And if +X points backwards, 180.
 #define HEADING_OFFSET 90.0
-// If your board is mounted upside-down in your robot, the heading results will go the
-// wrong way (i.e. increase anticlockwise). Set BOARD_INVERTED to fix this.
-#define BOARD_INVERTED false
 // The magnetometer produces readings based on magnetic north, whereas the HDT message
 // produced by this code should contain a heading based on true north. Enter your local
 // magnetic declination here to apply this offset. Positive declination is when mag
@@ -43,10 +42,22 @@
 // This code sends the heading data to the host and port specified here.
 #define UDP_SEND_SERVER "127.0.0.1"
 #define UDP_SEND_PORT 2021
+// Set the sample rate between 4 & 200 Hz. HDT messages will be sent at this rate. 10 Hz
+// recommended.
+#define SAMPLE_RATE_HZ 10
 // Set the I2C bus on which to communicate with the 9DOF MPU. For the Beaglebone Blue and
 // Beaglebone Black Robotics Cape, this is 2.
 #define I2C_BUS 2
+// Set a GPIO pin to use for an interrupt, in the DMP mode the MPU controls timing and
+// will interrupt us when it has new data
+#define GPIO_INT_PIN_CHIP 3
+#define GPIO_INT_PIN_PIN  21
 
+
+// Globals to pass data between threads
+rc_mpu_data_t data;
+int udpsocket;
+struct sockaddr_in server;
 
 // interrupt handler to catch ctrl-c
 static int running = 0;
@@ -55,10 +66,44 @@ static void __signal_handler(__attribute__ ((unused)) int dummy) {
     return;
 }
 
-int main()  {
-    // Struct to hold data
-    rc_mpu_data_t data;
+// Handle data function. Called back at a predefined interval by the MPU
+// when it has new data.
+static void __handle_data(void) {
+    // Calculate heading based on TaitBryan yaw reading
+    double heading = -data.fused_TaitBryan[TB_YAW_Z] * RAD_TO_DEG;
 
+    // Apply offsets
+    heading = heading + HEADING_OFFSET;
+    heading = heading + LOCAL_MAGNETIC_DECLINATION;
+
+    // Ensure we get a number in the range 0.0<=x<360.0
+    while (heading < 0.0) {
+        heading = heading + 360.0;
+    }
+    while (heading >= 360.0) {
+        heading = heading - 360.0;
+    }
+
+    // Build NMEA message content
+    char message[20], messageInner[14];
+    sprintf(messageInner, "GPHDT,%03.1f,T", heading);
+
+    // Calculate checksum
+    int crc = 0;
+    int i;
+    for (i = 0; i < 14; i ++) {
+        crc ^= messageInner[i];
+    }
+
+    // Assemble full message
+    sprintf(message, "$%s*%02X\r\n", messageInner, crc);
+
+    // Send packet
+    sendto(udpsocket, message, (strlen(message)+1), 0, (struct sockaddr *)&server, sizeof(server));
+}
+
+// Main function
+int main()  {
     // Set up interrupt handler
     signal(SIGINT, __signal_handler);
     running = 1;
@@ -66,17 +111,18 @@ int main()  {
     // Set up MPU config
     rc_mpu_config_t conf = rc_mpu_default_config();
     conf.i2c_bus = I2C_BUS;
+    conf.gpio_interrupt_pin_chip = GPIO_INT_PIN_CHIP;
+    conf.gpio_interrupt_pin = GPIO_INT_PIN_PIN;
     conf.enable_magnetometer = 1;
+    conf.dmp_sample_rate = SAMPLE_RATE_HZ;
 
     // Enable MPU, exit on failure
-    if (rc_mpu_initialize(&data, conf)){
-        fprintf(stderr,"rc_mpu_initialize failed\n");
+    if (rc_mpu_initialize_dmp(&data, conf)){
+        fprintf(stderr,"rc_mpu_initialize_dmp failed\n");
         return -1;
     }
 
     // Create UDP sockets, exit on failure
-    int udpsocket;
-    struct sockaddr_in server;
     if ((udpsocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) >= 0) {
         server.sin_family      = AF_INET;
         server.sin_port        = htons(UDP_SEND_PORT);
@@ -86,49 +132,12 @@ int main()  {
         return -1;
     }
 
-    // Main run loop
+    // Set the DMP callback method - the MPU will control the timing
+    // from now on.
+    rc_mpu_set_dmp_callback(&__handle_data);
+
+    // Wait until we need to quit, nothing else to do
     while (running) {
-        // Read X & Y mag data
-        rc_mpu_read_mag(&data);
-        double xField = data.mag[0];
-        double yField = data.mag[1];
-
-        // Calculate heading based on magnetometer reading
-        double heading = -atan2(-yField, xField)*(180/M_PI);
-
-        // Apply offsets and investions
-        if (BOARD_INVERTED) {
-            heading = -heading;
-        }
-        heading = heading + HEADING_OFFSET;
-        heading = heading + LOCAL_MAGNETIC_DECLINATION;
-
-        // Ensure we get a number in the range 0.0<=x<360.0
-        while (heading < 0.0) {
-            heading = heading + 360.0;
-        }
-        while (heading >= 360.0) {
-            heading = heading - 360.0;
-        }
-
-        // Build NMEA message content
-        char message[20], messageInner[14];
-        sprintf(messageInner, "GPHDT,%03.1f,T", heading);
-
-        // Calculate checksum
-        int crc = 0;
-        int i;
-        for (i = 0; i < 14; i ++) {
-            crc ^= messageInner[i];
-        }
-
-        // Assemble full message
-        sprintf(message, "$%s*%02X\r\n", messageInner, crc);
-
-        // Send packet
-        sendto(udpsocket, message, (strlen(message)+1), 0, (struct sockaddr *)&server, sizeof(server));
-
-        // 100ms delay until next time
         rc_usleep(100000);
     }
 
